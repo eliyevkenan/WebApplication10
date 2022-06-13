b817be9d1ed5277f777dcc4e43352fea6207dc5a
tu7gbzmx6 <tu7gbzmx6@215-9>
2022-06-13 20:36:39 +0400

    Add project files.

---
+++ WebApplication10/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication10.DAL;
+
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+ 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+ 
+    }
+}
